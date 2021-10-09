a2e5047e416cdc610b4b762700a8f5ec1b7034bf
moirahartigan <moirahartigan@gmail.com>
2021-10-09 16:36:03 +0000

    Instal and confirgure the allauth library

--- lets_cook_it_app/views.py
+++ lets_cook_it_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.views.generic  import TemplateView, CreateView
+# from .models import Categories, Recipe
+# from .forms import RegistrationForm
+# from django.urls import reverse_lazy
 
-# Create your views here.
+# class RecipeList(generic.ListView):
+#     model = Recipe
+#     queryset = Recipe.objects.filter(status=1)order_by('-created_on')
+#     template_name = 'index.html'
+#     )
+
+# class HomeView(TemplateView):
+#     template_name = 'lets_cook_it_app/home.html'
+
+# class RegistrationView(CreateView):
+#     from_class = RegistrationForm
+#     success_url = reverse_lazy()
