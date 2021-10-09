7c14646a2d43e7ac9cee6fdb9df6d97fa619c3e5
moirahartigan <moirahartigan@gmail.com>
2021-10-09 14:51:40 +0000

    Set up superuser to add recepies/categories

--- lets_cook_it_app/admin.py
+++ lets_cook_it_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Categories, Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Categories)
+class CategoriesAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('ingredients', 'method')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on', 'categories')
+    list_display = ('title', 'categories', 'author', 'status', 'created_on')
+    search_fields = ['title', 'ingredients', 'categories']
+    summernote_fields = ('ingredients', 'method')
+    actions = ['approve_recipes']
+
+    def approve_approve_recipes(self, request, queryset):
+        queryset.update(approved=True)
--- lets_cook_it_app/models.py
+++ lets_cook_it_app/models.py
@@ -4,30 +4,31 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Categories(models.Model):
     title = models.CharField(max_length=220, unique=True)
-    slug = models.SlugField(max_length=220, unique=True)
 
     def __str__(self):
         return self.title
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=220, unique=True)
     slug = models.SlugField(max_length=220, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipes')
     image = CloudinaryField('image', default='placeholder')
     ingredients = models.TextField()
     method = models.TextField()
-    prep_time = models.CharField(max_length=5, unique=True)
-    cook_time = models.CharField(max_length=5, unique=True)
-    servings = models.CharField(max_length=2, unique=True)
+    prep_time = models.CharField(max_length=5,)
+    cook_time = models.CharField(max_length=5,)
+    servings = models.CharField(max_length=2,)
     created_on = models.DateTimeField(auto_now_add=True)
-    Status = models.IntegerField(choices=STATUS, default=0)
-    categories = models.ForeignKey(Categories, on_delete=models.CASCADE) 
+    status = models.IntegerField(choices=STATUS, default=0)
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
 
-class Meta:
-    ordering = ['-created_on']
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
-
