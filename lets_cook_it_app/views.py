bf3de571399709e0c26dd36ba2c7d64201f04c9d
moirahartigan <moirahartigan@gmail.com>
2021-10-10 15:49:15 +0000

    Create view code for recipe card

--- lets_cook_it_app/views.py
+++ lets_cook_it_app/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
