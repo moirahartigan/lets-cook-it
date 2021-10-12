9316cdae200e831b8e9d761df339bbf6cc209cfc
moirahartigan <moirahartigan@gmail.com>
2021-10-12 12:54:41 +0000

    Build index.html page

--- lets_cook_it_app/urls.py
+++ lets_cook_it_app/urls.py
@@ -1,9 +1,8 @@
-# from . import views
-# from django.urls import path
+from . import views
+from django.urls import path
 
-# app_name = 'lets_cook_it_app'
+app_name = 'lets_cook_it_app'
 
-# # urlpatterns = [
-# #     path('', views.IndexView.as_view(), name='index'),
-    
-# # ]
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+]
--- recipebook/urls.py
+++ recipebook/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views import View
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
