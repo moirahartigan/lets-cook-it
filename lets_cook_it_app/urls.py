a5ba48e97a981945ce2bd0ab7067ee83319a1abe
moirahartigan <moirahartigan@gmail.com>
2021-10-12 13:52:02 +0000

    Wire up url's for single view recipes

--- lets_cook_it_app/urls.py
+++ lets_cook_it_app/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
 
-app_name = 'lets_cook_it_app'
+app_name = 'recipe'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('recipes/', views.RecipesView.as_view(), name='recipes'),
+    path('<slug:slug>/', views.SingleView.as_view(), name='single'),
+    
 ]
--- lets_cook_it_app/views.py
+++ lets_cook_it_app/views.py
@@ -1,5 +1,21 @@
-# from django.shortcuts import render, get_object_or_404
-# from django.views import generic, View
-# from .models import Recipe
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 
+class IndexView(ListView):
+    model = Recipe
+    template_name = 'index.html'
+    context_object_name = 'index'
+
+
+class SingleView(DetailView):
+    model = Recipe
+    template_name = 'single.html'
+    context_object_name = 'recipe'
+
+
+class RecipesView(ListView):
+    model = Recipe
+    template_name = 'recipes.html'
+    context_object_name = 'recipe_list'
