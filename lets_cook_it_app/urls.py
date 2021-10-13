840fa60f25dac7e9c0ec332b2271b6bfc1c44965
moirahartigan <moirahartigan@gmail.com>
2021-10-13 19:55:13 +0000

    Add recipe_detail View and wire url
    correct syntax errors in base.html and add link tags to recipes.html

--- lets_cook_it_app/urls.py
+++ lets_cook_it_app/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomeList.as_view(), name='home'),
     path('recipes/', views.RecipeList.as_view(), name='recipes'),
-    path('register/', views.RegisterPage.as_view(), name='register'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    # path('register/', views.RegisterPage.as_view(), name='register'),
     
 ]
--- lets_cook_it_app/views.py
+++ lets_cook_it_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -14,6 +14,22 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'recipes.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+    model = Recipe
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+            },
+        )
 
 
 class RegisterPage(generic.TemplateView):
