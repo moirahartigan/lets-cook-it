9630f8eec9aef585f59fd6bf986dcfbab1900306
moirahartigan <moirahartigan@gmail.com>
2021-10-17 11:39:19 +0000

    Add comments register decorator to admin.py
    Comments can now be approved from admin and views.py has been updated

--- lets_cook_it_app/admin.py
+++ lets_cook_it_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categories, Recipe
+from .models import Categories, Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -20,4 +20,15 @@ class RecipeAdmin(SummernoteModelAdmin):
     actions = ['approve_recipes']
 
     def approve_approve_recipes(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'message', 'recipe', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'message')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
         queryset.update(approved=True)
