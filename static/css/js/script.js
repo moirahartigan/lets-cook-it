bad22c01017eb9cecdb68728a6b7bed194bd5d87
moirahartigan <moirahartigan@gmail.com>
2021-10-25 14:05:30 +0000

    Add correct carousel layout on index and add  js

--- static/css/js/script.js
+++ static/css/js/script.js
@@ -1,25 +1,20 @@
-// Owl Carousel
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:true,
-                loop:false
-            }
-        }
-    })
+{% load static %}
 
-});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
 
