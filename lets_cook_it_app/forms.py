6670a9f8790684a70f48e0f685b0c3765955fa81
moirahartigan <moirahartigan@gmail.com>
2021-10-26 19:12:12 +0000

    Add summernote imports and recipe form to forms.py

--- lets_cook_it_app/forms.py
+++ lets_cook_it_app/forms.py
@@ -1,5 +1,19 @@
 from django import forms
-from .models import Comment, Categories
+from .models import Recipe, Comment, Categories
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+from django_summernote.fields import SummernoteTextField
+
+
+class RecipeForm(forms.ModelForm):
+
+    class Meta:
+        model = Recipe
+        widgets = {
+            'about': SummernoteWidget(),
+            'method': SummernoteWidget(),
+            'nutrition': SummernoteWidget(),
+            'ingredients': SummernoteWidget(),
+        }
 
 
 class RecipeCommentForm(forms.ModelForm):
