0b82bfb76a5143a3cc4a52e3642b46f069e2f3ce
moirahartigan <moirahartigan@gmail.com>
2021-10-30 13:17:21 +0000

    Remove registration image from small screen view

--- lets_cook_it_app/views.py
+++ lets_cook_it_app/views.py
@@ -98,9 +98,8 @@ class RecipeDetail(View):
 
 class RecipeLike(View):
 
-    def recipe(self, request, slug):
+    def recipe(self, request, slug, *args, **kwargs):
         recipe = get_object_or_404(Recipe, slug=slug)
-
         if recipe.likes.filter(id=request.user.id).exists():
             recipe.likes.remove(request.user)
         else:
